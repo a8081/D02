4e23fe488c1a7f1df7a2fff6e29fce443f560c0c
Jose Luis <joscasbla1@alum.us.es>
2019-03-11 18:12:00 +0100

    Repository, Service & populate

--- Item 3/Acme-Madruga/src/main/java/services/MiscellaneousRecordService.java
+++ Item 3/Acme-Madruga/src/main/java/services/MiscellaneousRecordService.java
@@ -1,12 +1,15 @@
 
 package services;
 
+import java.util.Collection;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.Assert;
 
 import repositories.MiscellaneousRecordRepository;
+import domain.MiscellaneousRecord;
 
 @Service
 @Transactional
