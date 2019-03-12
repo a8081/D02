49e7c1bbf92cf298a00ffa51aca8161ad292f4ed
a8081 <amartinezr98@protonmail.com>
2019-03-12 20:51:07 +0100

    creditCard to datatype

--- Item 3/Acme-Madruga/src/main/java/domain/CreditCard.java
+++ Item 3/Acme-Madruga/src/main/java/domain/CreditCard.java
@@ -4,16 +4,16 @@ package domain;
 import javax.persistence.Access;
 import javax.persistence.AccessType;
 import javax.persistence.Column;
-import javax.persistence.Entity;
+import javax.persistence.Embeddable;
 
 import org.hibernate.validator.constraints.CreditCardNumber;
 import org.hibernate.validator.constraints.NotBlank;
 import org.hibernate.validator.constraints.Range;
 import org.hibernate.validator.constraints.SafeHtml;
 
-@Entity
+@Embeddable
 @Access(AccessType.PROPERTY)
-public class CreditCard extends DomainEntity {
+public class CreditCard {
 
 	private String	holderName;
 	private String	make;
