6d1c4c176a3eae3a837ae398e9c76843351a31f6
Jose Luis <joscasbla1@alum.us.es>
2019-03-14 09:25:17 +0100

    History

---
+++ Item 3/Acme-Madruga/src/main/java/controllers/HistoryController.java
@@ -0,0 +1,55 @@
+
+package controllers;
+
+import java.util.Collection;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import services.BrotherhoodService;
+import services.HistoryService;
+import domain.Brotherhood;
+import domain.History;
+import domain.InceptionRecord;
+import domain.LegalRecord;
+import domain.LinkRecord;
+import domain.MiscellaneousRecord;
+import domain.PeriodRecord;
+
+@Controller
+@RequestMapping("/history")
+public class HistoryController extends AbstractController {
+
+	@Autowired
+	private HistoryService		historyService;
+
+	@Autowired
+	private BrotherhoodService	brotherhoodService;
+
+
+	@RequestMapping(value = "/list", method = RequestMethod.GET)
+	public ModelAndView list() {
+		final ModelAndView res;
+		final Brotherhood brotherhood = this.brotherhoodService.findByPrincipal();
+		final History history = brotherhood.getHistory();
+		final InceptionRecord inceptionRecord = history.getInceptionRecord();
+		final Collection<PeriodRecord> periodRecords = history.getPeriodRecords();
+		final Collection<LegalRecord> legalRecords = history.getLegalRecords();
+		final Collection<LinkRecord> linkRecords = history.getLinkRecords();
+		final Collection<MiscellaneousRecord> miscellaneousRecords = history.getMiscellaneousRecords();
+
+		res = new ModelAndView("history/list");
+		res.addObject("history", history);
+		res.addObject("inceptionRecord", inceptionRecord);
+		res.addObject("periodrecords", periodRecords);
+		res.addObject("legalRecord", legalRecords);
+		res.addObject("linkRecords", linkRecords);
+		res.addObject("miscellaneousRecords", miscellaneousRecords);
+
+		return res;
+	}
+
+}
