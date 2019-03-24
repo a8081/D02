0af4a4dfd598f3cb3d94cdd0fbe6e130b3e3189a
Jesús Manuel García Lanzas <jesgarlan@alum.us.es>
2019-03-24 16:44:42 +0100

    Indexación terminada

--- Item 3/Acme-Madruga/src/main/java/domain/Brotherhood.java
+++ Item 3/Acme-Madruga/src/main/java/domain/Brotherhood.java
@@ -9,8 +9,10 @@ import javax.persistence.AccessType;
 import javax.persistence.CascadeType;
 import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
+import javax.persistence.Index;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
+import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.Valid;
@@ -24,6 +26,9 @@ import cz.jirutka.validator.collection.constraints.EachURL;
 
 @Entity
 @Access(AccessType.PROPERTY)
+@Table(indexes = {
+	@Index(columnList = "area, history")
+})
 public class Brotherhood extends Actor {
 
 	private String				title;
--- Item 3/Acme-Madruga/src/main/java/domain/SocialProfile.java
+++ Item 3/Acme-Madruga/src/main/java/domain/SocialProfile.java
@@ -4,7 +4,9 @@ package domain;
 import javax.persistence.Access;
 import javax.persistence.AccessType;
 import javax.persistence.Entity;
+import javax.persistence.Index;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
 import org.hibernate.validator.constraints.NotBlank;
 import org.hibernate.validator.constraints.SafeHtml;
@@ -12,6 +14,9 @@ import org.hibernate.validator.constraints.URL;
 
 @Entity
 @Access(AccessType.PROPERTY)
+@Table(indexes = {
+	@Index(columnList = "actor")
+})
 public class SocialProfile extends DomainEntity {
 
 	private String	nick;
